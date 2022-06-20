7728682933029dfb16a7350657bb178fb5f17e8d
Lukas Baur <baurlukas89@gmail.com>
2022-06-20 19:30:30 +0200

    solved iteration 4 - 5

--- src/App.js
+++ src/App.js
@@ -5,11 +5,33 @@ import { useState } from "react";
 function App() {
   const [contacts, setContacts] = useState(contactsArray.slice(0, 5));
 
+  const removeContact = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
   const handleAddRandom = () => {
     const randomIndex = Math.floor(Math.random() * (contactsArray.length - 5));
     const contactsCopy = [...contacts];
     contactsCopy.push(contactsArray[5 + randomIndex]);
     setContacts(contactsCopy);
+  };
+
+  const sortContactByName = () => {
+    const contactsCopy = [...contacts];
+    setContacts(
+      contactsCopy.sort((contact1, contact2) =>
+        contact1.name.localeCompare(contact2.name)
+      )
+    );
+  };
+
+  const sortContactByPopularity = () => {
+    const contactsCopy = [...contacts];
+    setContacts(
+      contactsCopy.sort(
+        (contact1, contact2) => contact2.popularity - contact1.popularity
+      )
+    );
   };
 
   const displayContacts = () => {
@@ -27,6 +49,9 @@ function App() {
           <td>{contact.popularity}</td>
           <td>{contact.wonOscar ? " 🏆" : "No Oscars 💆‍♂️"}</td>
           <td>{contact.wonEmmy ? " 🏆" : "No Emmy 💆‍♂️"}</td>
+          <td>
+            <button onClick={() => removeContact(contact.id)}>Delete</button>
+          </td>
         </tr>
       );
     });
@@ -35,6 +60,10 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => handleAddRandom()}>Add Random Contact</button>
+      <button onClick={() => sortContactByName()}>Sort by Name</button>
+      <button onClick={() => sortContactByPopularity()}>
+        Sort by Popularity
+      </button>
       <table className="contactsTable">
         <tr>
           <th>Picture</th>
